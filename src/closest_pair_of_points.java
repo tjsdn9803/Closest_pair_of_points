643efdf6577e57a5f11dc6ce9cc0185c65803ae5
tjsdn9803 <leeinjoo2@inu.ac.kr>
2021-03-30 01:43:50 +0900

    ADD [CPc]

--- src/closest_pair_of_points.java
+++ src/closest_pair_of_points.java
@@ -1,4 +1,6 @@
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 class point{
     int x;
@@ -9,9 +11,10 @@ class point{
     }
 }
 public class closest_pair_of_points {
+    static int g,y;
     static point[] pointarr;
     static point[] closest2point;
-    static point[] middlepoints;
+    List<point> middlepoints;
     public double getdistance(point a,point b){
         return Math.sqrt((b.x-a.x)*(b.x-a.x)+(b.y-a.y)*(b.y-a.y));
     }
@@ -19,34 +22,56 @@ public class closest_pair_of_points {
         if(a<b) return a;
         else return b;
     }
+    public double min(double a,double b,double c){
+        if(a<b && a<c) return a;
+        else if(b<a && b<c) return b;
+        else return c;
+    }
 
     public point[] ClosestPair(point arr[],int p,int q){
         if(q-p<=2){
-            point a = pointarr[p];
-            point b = pointarr[q];
+            point a = arr[p];
+            point b = arr[q];
             if(q-p==1) return new point[]{a, b};
         }
 
         int k = (q+p)/2;
+
         point[] CPL = ClosestPair(arr,p,k);
         point[] CPR = ClosestPair(arr,k+1,q);
+        point[] CPC ;
         double DL = getdistance(CPL[0],CPL[1]);
         double DR = getdistance(CPR[0],CPR[1]);
-//
-//        double d = min(DL,DR);
-//        for(int w=0;w<q;w++){
-//            if(arr[w+p].x > arr[k].x-d && arr[w+p].x < arr[k+1].x + d){
-//                middlepoints = new point[q];
-//                middlepoints[w] = arr[w+p];
-//            }
-//        }
-//        point[] CPC = ClosestPair(middlepoints,p,q);
-//        double DC = getdistance(CPC[0],CPC[1]);
-//        if(DL<DR && DL<DC) return CPL;
-//        else if(DR<DL && DR<DC) return CPR;
-//        else return CPC;
-        if(DL<DR)return CPL;
-        else return CPR;
+        double DC = 10000;
+        double d = min(DL,DR);
+        middlepoints = new ArrayList<point>();
+        int s=0; //middlepoints배열의 크기가 고정되어있어서 에를들어 [4]로 선언되고 [0][1][2]만 초기화되면 [3]이 null상태 이상태에서 getdistance하면 에러
+        for(int h=0;h<(q-p+1);h++){//arr를 돌면서 좌측구역의 최우단점의 x좌표 -d보다 크고 우측구역의 최좌단의 x좌표+d보다 작은 점들을 middlepoints배열에 저장
+            if((arr[h+p].x) > (arr[k].x-d) && (arr[h+p].x) < (arr[k+1].x+d)){
+
+                middlepoints.add(arr[h+p]);
+            }
+        }
+        //왜 middlepoints[h] = arr[0]dl 들감??
+        CPC = new point[2];
+        for(int f=0;f<middlepoints.size();f++){
+            for(int t=f+1;t<middlepoints.size();t++){
+                double dis = getdistance(middlepoints.get(f),middlepoints.get(t));
+                if(DC > dis){
+                    DC = dis;
+//                    g = f+p;
+//                    y = t+p;
+                    CPC[0] = middlepoints.get(f);
+                    CPC[1] = middlepoints.get(t);
+                }
+            }
+        }
+        //CPC = new point[]{arr[g], arr[y]};
+        if(min(DL,DR,DC)==DL)return CPL;
+        else if(min(DL,DR,DC)==DR)return CPR;
+        else return CPC;
+//        if(DL<DR)return CPL;
+//        else return CPR;
 
 
     }
@@ -57,7 +82,8 @@ public class closest_pair_of_points {
 
         Scanner sc = new Scanner(System.in);
         System.out.println("점의 갯수 입력");
-        int n = sc.nextInt();
+//        int n = sc.nextInt();
+        int n =8;
         pointarr = new point[n];
 //        for (int i = 0; i < n; i++) {
 //            System.out.println(i + 1 + "번째 점의 x 입력");
@@ -69,7 +95,7 @@ public class closest_pair_of_points {
 //            pointarr[i] = p;
 //        }
         int xarr [] = {1,4,5 ,6,7,9,11,14};
-        int yarr [] = {1,3,11,2,2,4,3 ,1 };
+        int yarr [] = {1,3,11,2,2,4,4 ,1 };
         for(int b=0;b<n;b++){
             x = xarr[b];
             y = yarr[b];
@@ -91,8 +117,8 @@ public class closest_pair_of_points {
         int closestLy = closest2point[0].y;
         int closestRx = closest2point[1].x;
         int closestRy = closest2point[1].y;
-        System.out.println("(" + closestLx + "," + closestLy + ")");
-        System.out.println("와");
+        System.out.print("가장 가까운 두점은 (" + closestLx + "," + closestLy + ")");
+        System.out.print("와");
         System.out.println("(" + closestRx + "," + closestRy + ")");
 
     }
