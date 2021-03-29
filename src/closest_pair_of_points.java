81a2031180ff16d81289d6e277c1bdf4945d5b8b
tjsdn9803 <leeinjoo2@inu.ac.kr>
2021-03-29 22:00:22 +0900

    no CPc no 3points

--- src/closest_pair_of_points.java
+++ src/closest_pair_of_points.java
@@ -11,6 +11,7 @@ class point{
 public class closest_pair_of_points {
     static point[] pointarr;
     static point[] closest2point;
+    static point[] middlepoints;
     public double getdistance(point a,point b){
         return Math.sqrt((b.x-a.x)*(b.x-a.x)+(b.y-a.y)*(b.y-a.y));
     }
@@ -20,7 +21,6 @@ public class closest_pair_of_points {
     }
 
     public point[] ClosestPair(point arr[],int p,int q){
-        closest2point = new point[2];
         if(q-p<=2){
             point a = pointarr[p];
             point b = pointarr[q];
@@ -29,9 +29,25 @@ public class closest_pair_of_points {
 
         int k = (q+p)/2;
         point[] CPL = ClosestPair(arr,p,k);
-        point[] CPR = ClosestPair(arr,p+1,q);
-        double DL = getdistance(getdistance(CPL[0],CPL[1]);
-        double DR = getdistance(getdistance(CPR[0],CPR[1]);
+        point[] CPR = ClosestPair(arr,k+1,q);
+        double DL = getdistance(CPL[0],CPL[1]);
+        double DR = getdistance(CPR[0],CPR[1]);
+//
+//        double d = min(DL,DR);
+//        for(int w=0;w<q;w++){
+//            if(arr[w+p].x > arr[k].x-d && arr[w+p].x < arr[k+1].x + d){
+//                middlepoints = new point[q];
+//                middlepoints[w] = arr[w+p];
+//            }
+//        }
+//        point[] CPC = ClosestPair(middlepoints,p,q);
+//        double DC = getdistance(CPC[0],CPC[1]);
+//        if(DL<DR && DL<DC) return CPL;
+//        else if(DR<DL && DR<DC) return CPR;
+//        else return CPC;
+        if(DL<DR)return CPL;
+        else return CPR;
+
 
     }
 
@@ -43,19 +59,41 @@ public class closest_pair_of_points {
         System.out.println("점의 갯수 입력");
         int n = sc.nextInt();
         pointarr = new point[n];
-        for (int i = 0; i < n; i++) {
-            System.out.println(i + 1 + "번째 점의 x 입력");
-            x = sc.nextInt();
-            System.out.println(i + 1 + "번째 점의 y 입력");
-            y = sc.nextInt();
+//        for (int i = 0; i < n; i++) {
+//            System.out.println(i + 1 + "번째 점의 x 입력");
+//            x = sc.nextInt();
+//            System.out.println(i + 1 + "번째 점의 y 입력");
+//            y = sc.nextInt();
+//            point p = new point();
+//            p.setop(x, y);
+//            pointarr[i] = p;
+//        }
+        int xarr [] = {1,4,5 ,6,7,9,11,14};
+        int yarr [] = {1,3,11,2,2,4,3 ,1 };
+        for(int b=0;b<n;b++){
+            x = xarr[b];
+            y = yarr[b];
             point p = new point();
-            p.setop(x, y);
-            pointarr[i] = p;
+            p.setop(x,y);
+            pointarr[b] = p;
         }
         for (int j = 0; j < n; j++) {
             x = pointarr[j].x;
             y = pointarr[j].y;
             System.out.println("(" + x + "," + y + ")");
         }
+
+        closest_pair_of_points cpp = new closest_pair_of_points();
+        closest2point = new point[2];
+        closest2point = cpp.ClosestPair(pointarr,0,n-1);
+
+        int closestLx = closest2point[0].x;
+        int closestLy = closest2point[0].y;
+        int closestRx = closest2point[1].x;
+        int closestRy = closest2point[1].y;
+        System.out.println("(" + closestLx + "," + closestLy + ")");
+        System.out.println("와");
+        System.out.println("(" + closestRx + "," + closestRy + ")");
+
     }
 }
