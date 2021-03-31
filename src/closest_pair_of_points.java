258ad6af8ebcdc261d287c1cba3c19f417dc178e
tjsdn9803 <leeinjoo2@inu.ac.kr>
2021-04-01 01:54:46 +0900

    ADD [point.y filter]

--- src/closest_pair_of_points.java
+++ src/closest_pair_of_points.java
@@ -1,17 +1,29 @@
-
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
-class point{
+
+class point implements Comparable<point>{
     int x;
     int y;
     public void setop(int x,int y){
         this.x = x;
         this.y = y;
     }
+    public int getY(){
+        return this.y;
+    }
+    @Override
+    public int compareTo(point o) {
+        if(this.y < o.getY()){
+            return -1;
+        }else if(this.y > o.getY()){
+            return 1;
+        }
+        return 0;
+    }
 }
 public class closest_pair_of_points {
-    static int g,y;
     static point[] pointarr;
     static point[] closest2point;
     List<point> middlepoints;
@@ -38,9 +50,9 @@ public class closest_pair_of_points {
                 b= arr[p+1];
                 point c= arr[p+2];
                 double temp=min(getdistance(a,b), getdistance(a,c), getdistance(b,c));
+
                 if(temp == getdistance(a,b)) return new point[] {a,b};
                 else if(temp == getdistance(a,c)) return new point[] {a,c};
-                else return new point[] {b,c};
             }
         }
         int k = (q+p)/2;
@@ -54,22 +66,35 @@ public class closest_pair_of_points {
         double DC = 10000;
         double d = min(DL,DR);
         middlepoints = new ArrayList<point>();
-        int s=0; //middlepoints배열의 크기가 고정되어있어서 에를들어 [4]로 선언되고 [0][1][2]만 초기화되면 [3]이 null상태 이상태에서 getdistance하면 에러
+        //middlepoints배열의 크기가 고정되어있어서 에를들어 [4]로 선언되고 [0][1][2]만 초기화되면 [3]이 null상태 이상태에서 getdistance하면 에러
+        //동적배열로 선언하여 해결
         for(int h=0;h<(q-p+1);h++){//arr를 돌면서 좌측구역의 최우단점의 x좌표 -d보다 크고 우측구역의 최좌단의 x좌표+d보다 작은 점들을 middlepoints배열에 저장
-            if((arr[h+p].x) > (arr[k].x-d) && (arr[h+p].x) < (arr[k+1].x+d)){
-
+            if(((arr[h+p].x) > (arr[k].x-d)) && ((arr[h+p].x) < (arr[k+1].x+d))){
                 middlepoints.add(arr[h+p]);
+
             }
         }
-        //왜 middlepoints[h] = arr[0]dl 들감??
         CPC = new point[2];
-        for(int f=0;f<middlepoints.size();f++){
-            for(int t=f+1;t<middlepoints.size();t++){
-                double dis = getdistance(middlepoints.get(f),middlepoints.get(t));
-                if(DC > dis){
-                    DC = dis;
-                    CPC[0] = middlepoints.get(f);
-                    CPC[1] = middlepoints.get(t);
+//        for(int f=0;f<middlepoints.size();f++){
+//            for(int t=f+1;t<middlepoints.size();t++){
+//                double dis = getdistance(middlepoints.get(f),middlepoints.get(t));
+//                if(DC > dis){
+//                    DC = dis;
+//                    CPC[0] = middlepoints.get(f);
+//                    CPC[1] = middlepoints.get(t);
+//                }
+//            }
+//        }// 이 경우 middlepoints의 y축 분포가 넓으면 계산횟수가 n^2이므로 계산횟수가 급증함
+        Collections.sort(middlepoints);//오름차순정렬
+        for(int g=0;g<middlepoints.size();g++){
+            for(int e=g+1;e<middlepoints.size();e++){
+                if(d > ((middlepoints.get(g).getY())-(middlepoints.get(e).getY()))){//y좌표 차이가 d보다 클때는 계산하지 않음
+
+                    if(DC > getdistance(middlepoints.get(g),middlepoints.get(e))){
+                        DC = getdistance(middlepoints.get(g),middlepoints.get(e));
+                        CPC[0] = middlepoints.get(g);
+                        CPC[1] = middlepoints.get(e);
+                    }
                 }
             }
         }
@@ -95,15 +120,6 @@ public class closest_pair_of_points {
             p.setop(x, y);
             pointarr[i] = p;
         }
-//        int xarr [] = {0,1,4,5 ,6,7,9,11,14};
-//        int yarr [] = {1,1,3,11,2,3,4,4 ,1 };
-//        for(int b=0;b<n;b++){
-//            x = xarr[b];
-//            y = yarr[b];
-//            point p = new point();
-//            p.setop(x,y);
-//            pointarr[b] = p;
-//        }
         for (int j = 0; j < n; j++) {
             x = pointarr[j].x;
             y = pointarr[j].y;
@@ -121,6 +137,10 @@ public class closest_pair_of_points {
         System.out.print("가장 가까운 두점은 (" + closestLx + "," + closestLy + ")");
         System.out.print("와");
         System.out.println("(" + closestRx + "," + closestRy + ")");
+
+
+
+
 
     }
 }
